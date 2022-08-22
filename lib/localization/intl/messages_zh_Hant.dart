6233c0e50b7b983a3c1a4b8f11403d80e876c19f
Crizant Lai <crizant.lai@mpower.cloud>
2022-08-22 21:51:44 +0800

    fix: intl zh-hant

--- lib/localization/intl/messages_zh_Hant.dart
+++ lib/localization/intl/messages_zh_Hant.dart
@@ -20,37 +20,37 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'zh_Hant';
 
-  static String m0(value) => "此字段必須與${value}相符";
+  static String m0(value) => "此欄位必須與${value}相符";
 
-  static String m1(max) => "此字段必須小於或等於${max}";
+  static String m1(max) => "此欄位必須小於或等於${max}";
 
-  static String m2(maxLength) => "此字段的長度必須小於或等於${maxLength}";
+  static String m2(maxLength) => "此欄位的長度必須小於或等於${maxLength}";
 
-  static String m3(min) => "此字段必須大於或等於${min}";
+  static String m3(min) => "此欄位必須大於或等於${min}";
 
-  static String m4(minLength) => "此字段的長度必須大於或等於${minLength}";
+  static String m4(minLength) => "此欄位的長度必須大於或等於${minLength}";
 
-  static String m5(value) => "此字段不得等於${value}";
+  static String m5(value) => "此欄位不得等於${value}";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "creditCardErrorText":
-            MessageLookupByLibrary.simpleMessage("此字段需要有效的信用卡號碼。"),
+            MessageLookupByLibrary.simpleMessage("此欄位需要有效的信用卡號碼。"),
         "dateStringErrorText":
-            MessageLookupByLibrary.simpleMessage("此字段需要有效的日期字符串。"),
+            MessageLookupByLibrary.simpleMessage("此欄位需要有效的日期字符串。"),
         "emailErrorText":
-            MessageLookupByLibrary.simpleMessage("此字段需要有效的電子郵件地址。"),
+            MessageLookupByLibrary.simpleMessage("此欄位需要有效的電子郵件地址。"),
         "equalErrorText": m0,
-        "integerErrorText": MessageLookupByLibrary.simpleMessage("此字段需要有效的整數。"),
-        "ipErrorText": MessageLookupByLibrary.simpleMessage("此字段需要有效的IP。"),
-        "matchErrorText": MessageLookupByLibrary.simpleMessage("此字段與格式不匹配。"),
+        "integerErrorText": MessageLookupByLibrary.simpleMessage("此欄位需要有效的整數。"),
+        "ipErrorText": MessageLookupByLibrary.simpleMessage("此欄位需要有效的IP。"),
+        "matchErrorText": MessageLookupByLibrary.simpleMessage("此欄位與格式不匹配。"),
         "maxErrorText": m1,
         "maxLengthErrorText": m2,
         "minErrorText": m3,
         "minLengthErrorText": m4,
         "notEqualErrorText": m5,
-        "numericErrorText": MessageLookupByLibrary.simpleMessage("此字段必須是數字。"),
-        "requiredErrorText": MessageLookupByLibrary.simpleMessage("此字段不能為空。"),
-        "urlErrorText": MessageLookupByLibrary.simpleMessage("此字段需要有效的URL地址。")
+        "numericErrorText": MessageLookupByLibrary.simpleMessage("此欄位必須是數字。"),
+        "requiredErrorText": MessageLookupByLibrary.simpleMessage("此欄位不能為空。"),
+        "urlErrorText": MessageLookupByLibrary.simpleMessage("此欄位需要有效的URL地址。")
       };
 }
