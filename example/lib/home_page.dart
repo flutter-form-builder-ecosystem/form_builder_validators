f8b1ada87f8c6776fe5ceccefaab0eed472131cd
Martijn van Dijk <mhvdijk@gmail.com>
2024-05-08 14:15:22 +0200

    Fix issue

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   HomePageState createState() => HomePageState();
