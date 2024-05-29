4f24f871aae29d98960c2d9cfaa956af91ef5210
Martijn van Dijk <mhvdijk@gmail.com>
2024-05-29 18:53:56 +0200

    Add some extensions

--- lib/src/form_field_validator_extensions.dart
+++ lib/src/form_field_validator_extensions.dart
@@ -12,8 +12,42 @@ extension FormFieldValidatorExtensions<T> on FormFieldValidator<T> {
     return FormBuilderValidators.or([this, other]);
   }
 
+  /// Combines the current validator with another validator using logical XOR.
+  FormFieldValidator<T> not() {
+    return FormBuilderValidators.notEqual(this);
+  }
+
   /// Adds a condition to apply the validator only if the condition is met.
   FormFieldValidator<T> when(bool Function(T value) condition) {
     return FormBuilderValidators.conditional(condition, this);
+  }
+
+  /// Adds a condition to apply the validator only if the condition is not met.
+  FormFieldValidator<T> unless(bool Function(T value) condition) {
+    return FormBuilderValidators.conditional(
+        (value) => !condition(value), this);
+  }
+
+  /// Transforms the value before applying the validator.
+  FormFieldValidator<T> transform(T Function(T? value) transformer) {
+    return FormBuilderValidators.transform(this, transformer);
+  }
+
+  /// Skips the validator if the condition is met.
+  FormFieldValidator<T> skipWhen(bool Function(T? value) condition) {
+    return FormBuilderValidators.skipWhen(condition, this);
+  }
+
+  /// Logs the value during the validation process.
+  FormFieldValidator<T> log({String Function(T? value)? log}) {
+    return FormBuilderValidators.log(log: log);
+  }
+
+  /// Overrides the error message of the current validator.
+  FormFieldValidator<T> withMessage(String errorMessage) {
+    return (valueCandidate) {
+      final result = this(valueCandidate);
+      return result != null ? errorMessage : null;
+    };
   }
 }
