f97af06607473eef436f97a3257296b8c4e2e02b
Martijn van Dijk <mhvdijk@gmail.com>
2024-07-28 15:53:17 +0200

    Tests

--- test/src/numeric/between_validator_test.dart
+++ test/src/numeric/between_validator_test.dart
@@ -7,171 +7,352 @@ void main() {
   final String customErrorMessage = faker.lorem.sentence();
 
   group('BetweenValidator -', () {
-    test('should return null if the value is exactly at the minimum boundary',
-        () {
-      // Arrange
-      const num min = 5;
-      const num max = 10;
-      const BetweenValidator validator = BetweenValidator(min, max);
-      const num value = 5;
+    group('num', () {
+      test('should return null if the value is exactly at the minimum boundary',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<num> validator = BetweenValidator<num>(min, max);
+        const num value = 5;
 
-      // Act
-      final String? result = validator.validate(value);
+        // Act
+        final String? result = validator.validate(value);
 
-      // Assert
-      expect(result, isNull);
+        // Assert
+        expect(result, isNull);
+      });
+
+      test('should return null if the value is exactly at the maximum boundary',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<num> validator = BetweenValidator<num>(min, max);
+        const num value = 10;
+
+        // Act
+        final String? result = validator.validate(value);
+
+        // Assert
+        expect(result, isNull);
+      });
+
+      test('should return null if the value is within the range', () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<num> validator = BetweenValidator<num>(min, max);
+        const num value = 7;
+
+        // Act
+        final String? result = validator.validate(value);
+
+        // Assert
+        expect(result, isNull);
+      });
+
+      test(
+          'should return the default error message if the value is below the minimum boundary',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<num> validator = BetweenValidator<num>(min, max);
+        const num value = 4;
+
+        // Act
+        final String? result = validator.validate(value);
+
+        // Assert
+        expect(result, isNotNull);
+        expect(
+          result,
+          equals(
+            FormBuilderLocalizations.current.betweenErrorText(min, max),
+          ),
+        );
+      });
+
+      test(
+          'should return the default error message if the value is above the maximum boundary',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<num> validator = BetweenValidator<num>(min, max);
+        const num value = 11;
+
+        // Act
+        final String? result = validator.validate(value);
+
+        // Assert
+        expect(result, isNotNull);
+        expect(
+          result,
+          equals(
+            FormBuilderLocalizations.current.betweenErrorText(min, max),
+          ),
+        );
+      });
+
+      test(
+          'should return the custom error message if the value is below the minimum boundary',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        final BetweenValidator<num> validator = BetweenValidator<num>(
+          min,
+          max,
+          errorText: customErrorMessage,
+        );
+        const num value = 4;
+
+        // Act
+        final String? result = validator.validate(value);
+
+        // Assert
+        expect(result, equals(customErrorMessage));
+      });
+
+      test(
+          'should return the custom error message if the value is above the maximum boundary',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        final BetweenValidator<num> validator = BetweenValidator<num>(
+          min,
+          max,
+          errorText: customErrorMessage,
+        );
+        const num value = 11;
+
+        // Act
+        final String? result = validator.validate(value);
+
+        // Assert
+        expect(result, equals(customErrorMessage));
+      });
+
+      test(
+          'should return null when the value is null and null check is disabled',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<num> validator = BetweenValidator<num>(
+          min,
+          max,
+          checkNullOrEmpty: false,
+        );
+        const num? value = null;
+
+        // Act
+        final String? result = validator.validate(value);
+
+        // Assert
+        expect(result, isNull);
+      });
+
+      test('should return the default error message when the value is null',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<num> validator = BetweenValidator<num>(min, max);
+        const num? value = null;
+
+        // Act
+        final String? result = validator.validate(value);
+
+        // Assert
+        expect(result, isNotNull);
+        expect(
+          result,
+          equals(
+            FormBuilderLocalizations.current.betweenErrorText(min, max),
+          ),
+        );
+      });
     });
 
-    test('should return null if the value is exactly at the maximum boundary',
-        () {
-      // Arrange
-      const num min = 5;
-      const num max = 10;
-      const BetweenValidator validator = BetweenValidator(min, max);
-      const num value = 10;
+    group('String', () {
+      test('should return null if the value is exactly at the minimum boundary',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<String> validator =
+            BetweenValidator<String>(min, max);
+        const String value = '5';
 
-      // Act
-      final String? result = validator.validate(value);
+        // Act
+        final String? result = validator.validate(value);
 
-      // Assert
-      expect(result, isNull);
-    });
+        // Assert
+        expect(result, isNull);
+      });
 
-    test('should return null if the value is within the range', () {
-      // Arrange
-      const num min = 5;
-      const num max = 10;
-      const BetweenValidator validator = BetweenValidator(min, max);
-      const num value = 7;
+      test('should return null if the value is exactly at the maximum boundary',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<String> validator =
+            BetweenValidator<String>(min, max);
+        const String value = '10';
 
-      // Act
-      final String? result = validator.validate(value);
+        // Act
+        final String? result = validator.validate(value);
 
-      // Assert
-      expect(result, isNull);
-    });
+        // Assert
+        expect(result, isNull);
+      });
 
-    test(
-        'should return the default error message if the value is below the minimum boundary',
-        () {
-      // Arrange
-      const num min = 5;
-      const num max = 10;
-      const BetweenValidator validator = BetweenValidator(min, max);
-      const num value = 4;
+      test('should return null if the value is within the range', () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<String> validator =
+            BetweenValidator<String>(min, max);
+        const String value = '7';
 
-      // Act
-      final String? result = validator.validate(value);
+        // Act
+        final String? result = validator.validate(value);
 
-      // Assert
-      expect(result, isNotNull);
-      expect(
-        result,
-        equals(
-          FormBuilderLocalizations.current.betweenErrorText(min, max),
-        ),
-      );
-    });
+        // Assert
+        expect(result, isNull);
+      });
 
-    test(
-        'should return the default error message if the value is above the maximum boundary',
-        () {
-      // Arrange
-      const num min = 5;
-      const num max = 10;
-      const BetweenValidator validator = BetweenValidator(min, max);
-      const num value = 11;
+      test(
+          'should return the default error message if the value is below the minimum boundary',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<String> validator =
+            BetweenValidator<String>(min, max);
+        const String value = '4';
 
-      // Act
-      final String? result = validator.validate(value);
+        // Act
+        final String? result = validator.validate(value);
 
-      // Assert
-      expect(result, isNotNull);
-      expect(
-        result,
-        equals(
-          FormBuilderLocalizations.current.betweenErrorText(min, max),
-        ),
-      );
-    });
+        // Assert
+        expect(result, isNotNull);
+        expect(
+          result,
+          equals(
+            FormBuilderLocalizations.current.betweenErrorText(min, max),
+          ),
+        );
+      });
 
-    test(
-        'should return the custom error message if the value is below the minimum boundary',
-        () {
-      // Arrange
-      const num min = 5;
-      const num max = 10;
-      final BetweenValidator validator = BetweenValidator(
-        min,
-        max,
-        errorText: customErrorMessage,
-      );
-      const num value = 4;
+      test(
+          'should return the default error message if the value is above the maximum boundary',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<String> validator =
+            BetweenValidator<String>(min, max);
+        const String value = '11';
 
-      // Act
-      final String? result = validator.validate(value);
+        // Act
+        final String? result = validator.validate(value);
 
-      // Assert
-      expect(result, equals(customErrorMessage));
-    });
+        // Assert
+        expect(result, isNotNull);
+        expect(
+          result,
+          equals(
+            FormBuilderLocalizations.current.betweenErrorText(min, max),
+          ),
+        );
+      });
 
-    test(
-        'should return the custom error message if the value is above the maximum boundary',
-        () {
-      // Arrange
-      const num min = 5;
-      const num max = 10;
-      final BetweenValidator validator = BetweenValidator(
-        min,
-        max,
-        errorText: customErrorMessage,
-      );
-      const num value = 11;
+      test(
+          'should return the custom error message if the value is below the minimum boundary',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        final BetweenValidator<String> validator = BetweenValidator<String>(
+          min,
+          max,
+          errorText: customErrorMessage,
+        );
+        const String value = '4';
 
-      // Act
-      final String? result = validator.validate(value);
+        // Act
+        final String? result = validator.validate(value);
 
-      // Assert
-      expect(result, equals(customErrorMessage));
-    });
+        // Assert
+        expect(result, equals(customErrorMessage));
+      });
 
-    test('should return null when the value is null and null check is disabled',
-        () {
-      // Arrange
-      const num min = 5;
-      const num max = 10;
-      const BetweenValidator validator = BetweenValidator(
-        min,
-        max,
-        checkNullOrEmpty: false,
-      );
-      const num? value = null;
+      test(
+          'should return the custom error message if the value is above the maximum boundary',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        final BetweenValidator<String> validator = BetweenValidator<String>(
+          min,
+          max,
+          errorText: customErrorMessage,
+        );
+        const String value = '11';
 
-      // Act
-      final String? result = validator.validate(value);
+        // Act
+        final String? result = validator.validate(value);
 
-      // Assert
-      expect(result, isNull);
-    });
+        // Assert
+        expect(result, equals(customErrorMessage));
+      });
 
-    test('should return the default error message when the value is null', () {
-      // Arrange
-      const num min = 5;
-      const num max = 10;
-      const BetweenValidator validator = BetweenValidator(min, max);
-      const num? value = null;
+      test(
+          'should return null when the value is null and null check is disabled',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<String> validator = BetweenValidator<String>(
+          min,
+          max,
+          checkNullOrEmpty: false,
+        );
+        const String? value = null;
 
-      // Act
-      final String? result = validator.validate(value);
+        // Act
+        final String? result = validator.validate(value);
 
-      // Assert
-      expect(result, isNotNull);
-      expect(
-        result,
-        equals(
-          FormBuilderLocalizations.current.betweenErrorText(min, max),
-        ),
-      );
+        // Assert
+        expect(result, isNull);
+      });
+
+      test('should return the default error message when the value is null',
+          () {
+        // Arrange
+        const num min = 5;
+        const num max = 10;
+        const BetweenValidator<String> validator =
+            BetweenValidator<String>(min, max);
+        const String? value = null;
+
+        // Act
+        final String? result = validator.validate(value);
+
+        // Assert
+        expect(result, isNotNull);
+        expect(
+          result,
+          equals(
+            FormBuilderLocalizations.current.betweenErrorText(min, max),
+          ),
+        );
+      });
     });
   });
 }
