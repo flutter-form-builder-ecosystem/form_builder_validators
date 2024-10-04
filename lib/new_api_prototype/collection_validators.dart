c4dd41d0db0f5f5f61bd3a1d0b4d4820a1f6fe8f
Artur Assis Alves <arturassiscomp@gmail.com>
2024-10-03 23:04:45 -0300

    Implement equal length validator

--- lib/new_api_prototype/collection_validators.dart
+++ lib/new_api_prototype/collection_validators.dart
@@ -34,3 +34,36 @@ Validator<T> maxLength<T extends Object>(int maxLength,
         : null;
   };
 }
+
+/// Returns a [Validator] function that checks if its input has the length equals
+/// to the provided positional parameter `length`. If the input satisfies this
+/// condition, the validator returns `null`. Otherwise, it returns the default
+/// error message `FormBuilderLocalizations.current.equalLengthErrorText(length)`,
+/// if [equalLengthMsg] is not provided.
+///
+/// # Caveats
+/// - Objects that are not collections are considered as collections with
+/// length 1.
+///
+/// # Errors
+/// - Throws [AssertionError] if `length` is negative.
+/// - Throws [AssertionError] if `allowEmpty` is `false` and `length` is 0.
+Validator<T> equalLength<T extends Object>(int length,
+    {bool allowEmpty = false, String Function(int)? equalLengthMsg}) {
+  assert(length >= 0, 'The length parameter may not be negative');
+  assert(!(!allowEmpty && length == 0),
+      'If empty is not allowed, the target length may not be 0.');
+  final String errorMsg = equalLengthMsg?.call(length) ??
+      FormBuilderLocalizations.current.equalLengthErrorText(length);
+  return (T value) {
+    // I think it makes more sense to say that scalar objects has length 1 instead of 0.
+    int valueLength = 1;
+
+    if (value is String) valueLength = value.length;
+    if (value is Iterable) valueLength = value.length;
+    if (value is Map) valueLength = value.length;
+    return valueLength == length || (allowEmpty && valueLength == 0)
+        ? null
+        : errorMsg;
+  };
+}
---
+++ test/new_api_testing/collection_validators/equal_length_validator_test.dart
@@ -0,0 +1,72 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+
+void main() {
+  group('Validator: equalLength', () {
+    group('Validations with default error message', () {
+      test('Should validate the input when it is expected to have length 0',
+          () {
+        final Validator<List<int>> v = equalLength(0, allowEmpty: true);
+        final Validator<Object> v2 = equalLength(0, allowEmpty: true);
+
+        expect(v(<int>[]), isNull);
+        expect(v2(<int>[12]),
+            FormBuilderLocalizations.current.equalLengthErrorText(0));
+        expect(
+            v2(12), FormBuilderLocalizations.current.equalLengthErrorText(0));
+      });
+
+      test(
+          'Should validate the input when it is expected to have length 1 and does not allow empty',
+          () {
+        final Validator<List<int>> v = equalLength(1);
+        final Validator<Object> v2 = equalLength(1);
+
+        expect(v(<int>[0]), isNull);
+        expect(v2(<String>['0']), isNull);
+        expect(v2(0), isNull);
+        expect(v(<int>[]),
+            FormBuilderLocalizations.current.equalLengthErrorText(1));
+        expect(v2(<int>[]),
+            FormBuilderLocalizations.current.equalLengthErrorText(1));
+        expect(v(<int>[1, 2]),
+            FormBuilderLocalizations.current.equalLengthErrorText(1));
+        expect(v2(<Object>[1, '3']),
+            FormBuilderLocalizations.current.equalLengthErrorText(1));
+      });
+      test(
+          'Should validate the input when it is expected to have length 4 and it allows empty',
+          () {
+        final Validator<List<String>> v = equalLength(4, allowEmpty: true);
+        final Validator<String> v2 = equalLength(4, allowEmpty: true);
+
+        expect(v(<String>['1', '2', '3', '4']), isNull);
+        expect(v(<String>[]), isNull);
+        expect(v(<String>['2', '3', '4']),
+            FormBuilderLocalizations.current.equalLengthErrorText(4));
+
+        expect(v2('1234'), isNull);
+        expect(v2(''), isNull);
+        expect(v2('123'),
+            FormBuilderLocalizations.current.equalLengthErrorText(4));
+      });
+    });
+
+    test('Should validate input returning custom message error when invalid',
+        () {
+      final customMsg = 'custom msg';
+      final v = equalLength(3, equalLengthMsg: (_) => customMsg);
+
+      expect(v('hey'), isNull);
+      expect(v([1, '2', 3, 4]), equals(customMsg));
+    });
+    test('Should throw AssertionError when length is negative', () {
+      expect(() => equalLength(-2), throwsAssertionError);
+    });
+    test(
+        'Should throw AssertionError when length is 0 and empty is not allowed',
+        () {
+      expect(() => equalLength(0), throwsAssertionError);
+    });
+  });
+}
