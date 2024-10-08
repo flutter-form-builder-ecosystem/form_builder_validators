408a9d47b6f3d91ee8b4d6569b39a69867268fc6
Artur Assis Alves <arturassiscomp@gmail.com>
2024-10-08 20:44:28 -0300

    implement conditional validator skip if

--- lib/new_api_prototype/core_validators/conditional_validators.dart
+++ lib/new_api_prototype/core_validators/conditional_validators.dart
@@ -7,3 +7,12 @@ Validator<T> validateIf<T extends Object?>(
     bool Function(T value) condition, Validator<T> v) {
   return (T value) => condition(value) ? v(value) : null;
 }
+
+/// This function returns a validator that conditionally skips validation for
+/// the user input. If `condition` applied to the user input evaluates to true,
+/// the validation step is skipped and null is returned. Otherwise, it is
+/// validated by 'v'.
+Validator<T> skipIf<T extends Object?>(
+    bool Function(T value) condition, Validator<T> v) {
+  return (T value) => condition(value) ? null : v(value);
+}
