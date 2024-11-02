b58c851b6d087a340f2258cd0b877afe6f4b5b37
Artur Assis Alves <arturassiscomp@gmail.com>
2024-11-02 10:46:10 -0300

    implement isNotEqual

--- lib/new_api_prototype/core_validators/equality_validators.dart
+++ lib/new_api_prototype/core_validators/equality_validators.dart
@@ -2,14 +2,10 @@ import '../../localization/l10n.dart';
 import '../constants.dart';
 
 /// This function returns a validator that checks if the user input is equal (using
-/// the == operator) to either `value`. If the condition is satisfied,
+/// the == operator) to `value`. If the condition is satisfied,
 /// the validator returns null, otherwise, it returns
 /// `FormBuilderLocalizations.current.equalErrorText(userInput.toString())`, if
-/// `equalMsg` is not provided.
-///
-/// # Error
-/// - Throws [AssertionError] if not exactly one of `value` or `dynValue` was
-/// provided.
+/// `isEqualMsg` is not provided.
 Validator<T> isEqual<T extends Object?>(
   T value, {
   String Function(String)? isEqualMsg,
@@ -20,5 +16,23 @@ Validator<T> isEqual<T extends Object?>(
         ? null
         : isEqualMsg?.call(valueString) ??
             FormBuilderLocalizations.current.equalErrorText(valueString);
+  };
+}
+
+/// This function returns a validator that checks if the user input is not equal (using
+/// the != operator) to `value`. If the condition is satisfied,
+/// the validator returns null, otherwise, it returns
+/// `FormBuilderLocalizations.current.notEqualErrorText(userInput.toString())`, if
+/// `isNotEqualMsg` is not provided.
+Validator<T> isNotEqual<T extends Object?>(
+  T value, {
+  String Function(String)? isNotEqualMsg,
+}) {
+  return (T input) {
+    final String valueString = value.toString();
+    return value != input
+        ? null
+        : isNotEqualMsg?.call(valueString) ??
+            FormBuilderLocalizations.current.notEqualErrorText(valueString);
   };
 }
